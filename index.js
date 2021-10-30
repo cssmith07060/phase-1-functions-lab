2c00bb2fd5fe4776c88079dc1a84904cd17b87d8
cssmith07060 <cssmith07060@gmail.com>
2021-10-30 06:07:41 -0400

    commit tree 91ab35b2b50a0de7b085cd137414786a570dfe26

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+  
+  if (blockNumber > 42) {
+    
+    return blockNumber - 42
+  
+  } else {
+    
+    return 42 - blockNumber
+  }
+  
+}
+function distanceFromHqInfeet(blockNumber) {
+  
+  return distanceFromHqInBlocks(blockNumber) * 264;
+
+}
+function distanceTravelledInFeet(startigBlock, endingBlock) {
+  
+  if (startingBlock > endingBlock) {
+    
+    return (startingBlock - endingBlock) * 264;
+  
+  } else {
+    
+    return (endingBlock - startigBlock) * 264;
+  } 
+
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInfeet(start, destination);
+
+  if (distance <= 400) {
+    return 0; 
+  } 
+}
+
+
+  
+
+
